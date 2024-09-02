bd2b4f96f22801d230b11f4d7118ec97993ad6d3
Flavia1892 <flavia.bobeica@live.com>
2024-09-02 08:09:08 +0300

    priceUpdates

--- src/Pages/Price.js
+++ src/Pages/Price.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import mockDataPrices from "../props/mockDataPrices";
 
 function Price() {
+  let prices = mockDataPrices.prices[0];
+
   return (
-    <div>Price</div>
-  )
+    <>
+      <p class="text-4xl text-center font-bold font-serif pt-3 pb-3">
+        {" "}
+        {prices.title} prices:
+      </p>
+      <div class="container bg-yellow-100">
+        <div class="col bg-red-200 mb-3 colPrice">
+          <div class="bg-blue-100">
+            Daily morning: {prices.price.daily_morning}.....................${" "}
+          </div>
+          <div>
+            Daily afternoon: {prices.price.daily_afternoon}..................${" "}
+          </div>
+          <div>
+            Daily evening: {prices.price.daily_evening}.....................${" "}
+          </div>
+          <div>Bi-Daily: {prices.price.bidaily}.....................$ </div>
+          <div>1 week full: {prices.price.week}.....................$ </div>
+          <div>
+            1 month full or more: {prices.price.month}
+            .....................$/month{" "}
+          </div>
+          <div>Minimum visit duration: {prices.price.duration_hours} hours</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Price
+export default Price;
--- src/props/mockDataPrices.js
+++ src/props/mockDataPrices.js
@@ -3,5 +3,19 @@ import React from "react";
 let data;
 
 export default data = {
-  prices: [{ title: "HouseSitting", price: "30$ per day" , id:101}],
+  prices: [
+    {
+      title: "House Sitting",
+      price: {
+        daily_morning: 30,
+        daily_afternoon: 40,
+        daily_evening: 50,
+        bidaily: 45,
+        duration_hours: 3,
+        week: 200,
+        month:700,
+      },
+      id: 101,
+    },
+  ],
 };
